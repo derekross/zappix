f739fec7df8e3f85c85415f5d090569fb19f0d55
Nich Secord <github@secord.io>
2025-04-26 17:28:24 -0700

    deps(tailwind): Install tailwind and related deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
-// vite.config.ts
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   optimizeDeps: {
-    // Add this section
-    exclude: ["nostr-tools"], // Exclude nostr-tools from pre-bundling
+    exclude: ["nostr-tools"],
   },
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
 });
