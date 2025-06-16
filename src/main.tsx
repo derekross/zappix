e5094ca04e53e6d2abb4b134ab497b6d1fd43a06
Derek Ross <derekross@gmail.com>
2025-06-15 23:14:08 -0400

    feat: PWA

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { createRoot } from 'react-dom/client';
+import { createRoot } from "react-dom/client";
 
 // Import polyfills first
-import './lib/polyfills.ts';
+import "./lib/polyfills.ts";
 
-import App from './App.tsx';
-import './index.css';
+import App from "./App.tsx";
+import "./index.css";
+import { registerServiceWorker } from "./lib/registerSW";
+
+// Register service worker for PWA support
+registerServiceWorker();
 
 // Using system fonts for optimal performance
 
